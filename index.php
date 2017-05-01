22927b9a8a0e998df733bbc11fc2886fd5c763b2
freda46 <fredathink@gmail.com>
2017-05-01 23:16:43 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="mahasiswa">
+    <meta name="author" content="Freda Rizky Saputra">
+
+    <title>OOP 2014150136</title>
+    <!-- favicon -->
+    <link rel="shortcut icon" href="assets/img/favicon.png">
+
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/datepicker.min.css" rel="stylesheet">
+    <link href="assets/css/landing-page.css" rel="stylesheet">
+    <link href="assets/js/dataTables/css/dataTables.bootstrap.css" rel="stylesheet">
+    
+    <!-- styles -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation" style="background-color: #fff">
+        <div class="container topnav">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand topnav" href="#" style="color: #555">OOP 2014150136</a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="http://www.fredazip.blogspot.com" target="_blank">Freda Rizky Saputra</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <a name="about"></a>
+    <div class="intro-header">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="intro-message">
+                        <h2> UNIVERSITAS SAINS AL-QUR'AN </h2>
+                        <h3> Data Mahasiswa Teknik Informatika 2014</h3>
+                        <hr class="intro-divider">
+                        <ul class="list-inline intro-social-buttons">
+                            <li>
+                                <a href="indexe.php" data-toggle="modal" class="btn btn-warning btn-lg"><i class="fa fa-user fa-fw"></i> <span class="network-name">MASUK</span></a>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+            
+
+ <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
